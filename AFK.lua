a895c9b531d0e9051776c43a85b1073d88662304
HerolsNero <111847461+HerolsNero@users.noreply.github.com>
2023-02-24 20:14:14 -0500

    Update AFK.lua

--- AFK.lua
+++ AFK.lua
@@ -8,12 +8,12 @@ if game.PlaceId == 5411459567 then
         task.wait(1)
     until game.Workspace.Message
 end
-_G.MinTimer = 80
+_G.MinTimer = 82
 
 local function notify(text)
     game.StarterGui:SetCore("SendNotification", {
         Title = "Dect time",
-        Duration = 80,
+        Duration = 82,
         Text = text
     })
 end
@@ -31,12 +31,12 @@ end
 
 if game.GameId == 578392296 then
     if game.PlaceId ~= 5411459567 then
-        notify("Joining AFK World, please")
+        notify("Joining AFK World!!")
         serverHop()
         game:GetService('TeleportService'):Teleport(5411459567)
     end
 
-    notify("AFK Hop running")
+    notify("AFK On")
     local function timerCheck()
         local timer = game.Workspace.Message.Text:split(" ")
         local time = tonumber(timer[1])
@@ -44,16 +44,16 @@ if game.GameId == 578392296 then
     end
 
       if timerCheck() <= _G.MinTimer then
-        notify("Waiting for cash")
+        notify("Waiting for Cash")
         repeat
             task.wait()
         until timerCheck() > _G.MinTimer
-        notify("Server Hopping")
+        notify("Alr time to dip")
         task.wait(1)
         serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HerolsNero/ABAs/main/Hopped.lua", true))()
     else
-        notify("Server Hopping")
+        notify("Alr time to dip")
         serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HerolsNero/ABAs/main/Hopped.lua", true))()
     end
