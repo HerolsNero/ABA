71a853d36091afe0463fd1b644bfc3c4911636a7
HerolsNero <111847461+HerolsNero@users.noreply.github.com>
2023-02-27 19:47:48 -0500

    Update AFK.lua

--- AFK.lua
+++ AFK.lua
@@ -1,22 +1,15 @@
 repeat
     wait()
 until game.Loaded
-game:GetService("RunService"):Set3dRenderingEnabled(false)
-task.wait(.1)
+game:GetService("RunService"):Set3dRenderingEnabled(true)
+task.wait(0.1)
 if game.PlaceId == 5411459567 then
     repeat
-        task.wait(.1)
+        task.wait(0.1)
     until game.Workspace.Message
 end
-_G.MinTimer = 80
+_G.MinTimer = 85
 
-local function notify(text)
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "Looking at the time yk yk",
-        Duration = 80,
-        Text = text
-    })
-end
 
 
 local function serverHop()
@@ -32,9 +25,10 @@ end
 
 if game.GameId == 578392296 then
     if game.PlaceId ~= 5411459567 then
-serverHop()
+        serverHop()
         game:GetService('TeleportService'):Teleport(5411459567)
     end
+
   
     local function timerCheck()
         local timer = game.Workspace.Message.Text:split(" ")
@@ -43,18 +37,17 @@ serverHop()
     end
 
       if timerCheck() <= _G.MinTimer then
- notify("Waiting for Cash")
+ 
         repeat
             task.wait()
         until timerCheck() > _G.MinTimer
-        notify("Alr time to dip")
         task.wait(0.1)
         serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HerolsNero/ABAs/main/Hopped.lua", true))()
     else
-        notify("Alr time to dip")
         serverHop()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HerolsNero/ABAs/main/Hopped.lua", true))()
     end
 
 end
+Performance()
